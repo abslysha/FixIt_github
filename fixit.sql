2c744400e0c9361fdf47cc8df49cd67f80b3d22c
amsyar7806 <m.amsyar7806@gmail.com>
2026-06-21 18:13:36 +0800

    add files

--- fixit.sql
+++ fixit.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 06, 2026 at 10:57 AM
+-- Generation Time: Jun 21, 2026 at 12:10 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,10 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `adminID` varchar(50) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`adminID`, `name`, `email`, `password`) VALUES
+('A001', 'alifah', 'alifah@gmail.com', '$2y$10$L8XIlzSKzt9QqGFhAQlcL./nEK7C4biV5m6rfcCn2mvRBNqzJtJOS');
 
 -- --------------------------------------------------------
 
@@ -42,10 +49,17 @@ CREATE TABLE `admin` (
 
 CREATE TABLE `maintenance` (
   `staffID` varchar(50) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `maintenance`
+--
+
+INSERT INTO `maintenance` (`staffID`, `name`, `email`, `password`) VALUES
+('S001', 'faziq', 'faziq@gmail.com', '$2y$10$RSEKFav/4qe5t3lYWstcveslCcKumKwz1sY6kN/yIXEvyFgU/JEGy');
 
 -- --------------------------------------------------------
 
@@ -55,15 +69,27 @@ CREATE TABLE `maintenance` (
 
 CREATE TABLE `report` (
   `reportID` varchar(100) NOT NULL,
+  `adminID` varchar(50) DEFAULT NULL,
+  `userID` varchar(50) NOT NULL,
+  `staffID` varchar(50) DEFAULT NULL,
+  `title` varchar(150) DEFAULT NULL,
   `description` text DEFAULT NULL,
-  `dateReported` datetime DEFAULT current_timestamp(),
+  `DateReported` datetime DEFAULT current_timestamp(),
   `status` enum('Pending','In Progress','Completed') DEFAULT 'Pending',
   `location` varchar(255) DEFAULT NULL,
-  `attachment` varchar(255) DEFAULT NULL,
-  `userID` varchar(100) DEFAULT NULL,
-  `adminID` varchar(100) DEFAULT NULL,
-  `staffID` varchar(100) DEFAULT NULL
+  `attachment` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `report`
+--
+
+INSERT INTO `report` (`reportID`, `adminID`, `userID`, `staffID`, `title`, `description`, `DateReported`, `status`, `location`, `attachment`) VALUES
+('R001', NULL, 'U002', 'S001', 'pc problem', 'bang pc ni tkle on', '2026-06-21 17:23:03', 'Pending', 'bilik dekan', 'uploads/report_6a37ad770b7d12.07346785.jpeg'),
+('R002', NULL, 'U002', 'S001', 'plug rosak', 'rosak rosak', '2026-06-21 17:39:14', 'Completed', 'depan bilik ficts', 'uploads/report_6a37b142ae70f8.44003598.jpeg'),
+('R003', NULL, 'U002', 'S001', 'cable rosak', 'cable disediakan rosak', '2026-06-21 17:41:49', 'In Progress', 'mm3', 'uploads/report_6a37b1dd01e264.47666149.jpeg'),
+('R004', NULL, 'U002', NULL, 'wifi down', 'wifi couldnt connect', '2026-06-21 18:03:17', 'Pending', 'bk3', 'uploads/report_6a37b6e523dcd9.86847086.jpeg'),
+('R005', NULL, 'U002', NULL, 'broken window', 'need to be fix asap', '2026-06-21 18:04:12', 'Pending', 'bk1', 'uploads/report_6a37b71c161609.63696102.jpeg');
 
 -- --------------------------------------------------------
 
@@ -73,10 +99,18 @@ CREATE TABLE `report` (
 
 CREATE TABLE `user` (
   `userID` varchar(50) NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userID`, `name`, `email`, `password`) VALUES
+('U001', 'amsyar', 'amsyar@gmail.com', '$2y$10$mWZPR3bFAeeYO98sBURTaeR7UGHxxQWeUtIrOqDC8zOWRoirMMjl6'),
+('U002', 'abby', 'abby@gmail.com', '$2y$10$9nC7VrYcSI3wQbFPFLPz1un/6CwyWMqhe6OV1sEfqoDa2iNzej1.2');
 
 --
 -- Indexes for dumped tables
@@ -86,28 +120,31 @@ CREATE TABLE `user` (
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
-  ADD PRIMARY KEY (`adminID`);
+  ADD PRIMARY KEY (`adminID`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `maintenance`
 --
 ALTER TABLE `maintenance`
-  ADD PRIMARY KEY (`staffID`);
+  ADD PRIMARY KEY (`staffID`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Indexes for table `report`
 --
 ALTER TABLE `report`
   ADD PRIMARY KEY (`reportID`),
-  ADD KEY `userID` (`userID`),
   ADD KEY `adminID` (`adminID`),
+  ADD KEY `userID` (`userID`),
   ADD KEY `staffID` (`staffID`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`userID`);
+  ADD PRIMARY KEY (`userID`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
 -- Constraints for dumped tables
@@ -117,8 +154,8 @@ ALTER TABLE `user`
 -- Constraints for table `report`
 --
 ALTER TABLE `report`
-  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
-  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`adminID`) REFERENCES `admin` (`adminID`),
+  ADD CONSTRAINT `report_ibfk_1` FOREIGN KEY (`adminID`) REFERENCES `admin` (`adminID`),
+  ADD CONSTRAINT `report_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`),
   ADD CONSTRAINT `report_ibfk_3` FOREIGN KEY (`staffID`) REFERENCES `maintenance` (`staffID`);
 COMMIT;
 
